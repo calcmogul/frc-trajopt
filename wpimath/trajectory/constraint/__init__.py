3d49c0410c43fe4e56c71cfb665806f85226b8e2
Tyler Veness <calcmogul@gmail.com>
2022-03-17 19:19:43 -0700

    Add MaxAccelerationConstraint

--- wpimath/trajectory/constraint/__init__.py
+++ wpimath/trajectory/constraint/__init__.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+from wpimath.system import LinearSystem
+
 
 class TrajectoryConstraint:
     __metaclass__ = ABCMeta
@@ -21,7 +23,7 @@ class CentripetalAccelerationConstraint(TrajectoryConstraint):
         # a = (v_l + v_r) / 2 * (v_r - v_l) / trackwidth
         # a = (v_r + v_l)(v_r - v_l) / (2 trackwidth)
         # a = (v_r² - v_l²) / (2 trackwidth)
-        return opti.subject_to(
+        opti.subject_to(
             opti.bounded(
                 -self.max_acceleration,
                 (X[4, :] ** 2 - X[3, :] ** 2) / (2 * self.trackwidth),
@@ -34,6 +36,17 @@ class MaxVelocityConstraint(TrajectoryConstraint):
     def __init__(self, max_velocity: float):
         self.max_velocity = max_velocity
 
-    def apply(self, opti, X, U):
+    def apply(self, opti, X, U) -> None:
         v = (X[3, :] + X[4, :]) / 2
-        return opti.subject_to(opti.bounded(-self.max_velocity, v, self.max_velocity))
+        opti.subject_to(opti.bounded(-self.max_velocity, v, self.max_velocity))
+
+
+class MaxAccelerationConstraint(TrajectoryConstraint):
+    def __init__(self, system: LinearSystem, max_acceleration: float):
+        self.system = system
+        self.max_acceleration = max_acceleration
+
+    def apply(self, opti, X, U) -> None:
+        dxdt = self.system.A @ X[3:5, :] + self.system.B @ U
+        a = (dxdt[0, :] + dxdt[1, :]) / 2
+        opti.subject_to(opti.bounded(-self.max_acceleration, a, self.max_acceleration))
