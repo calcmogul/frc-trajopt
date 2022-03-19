91a0a4823b41ed770aced18f0637ed4ad8159563
Tyler Veness <calcmogul@gmail.com>
2022-03-18 17:23:17 -0700

    Produce initial guess with differential drive linear time-varying LQR

--- wpimath/trajectory/__init__.py
+++ wpimath/trajectory/__init__.py
@@ -10,6 +10,23 @@ from wpimath.system import LinearSystem
 from wpimath.trajectory.constraint import *
 
 
+def runge_kutta(f, x, u, dt):
+    """Fourth order Runge-Kutta integration.
+
+    Keyword arguments:
+    f -- vector function to integrate
+    x -- vector of states
+    u -- vector of inputs (constant for dt)
+    dt -- time for which to integrate
+    """
+    half_dt = dt * 0.5
+    k1 = f(x, u)
+    k2 = f(x + half_dt * k1, u)
+    k3 = f(x + half_dt * k2, u)
+    k4 = f(x + dt * k3, u)
+    return x + dt / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+
 class DifferentialDriveTrajectoryOptimizer:
     class Waypoint:
         def __init__(
@@ -61,6 +78,8 @@ class DifferentialDriveTrajectoryOptimizer:
         ]
         self.constraints = []  # List of TrajectoryConstraints
 
+        self.vars_per_segment = 100
+
     def add_pose(
         self, pose: Pose2d, constraints: List[TrajectoryConstraint] = None
     ) -> None:
@@ -102,6 +121,97 @@ class DifferentialDriveTrajectoryOptimizer:
         """
         self.constraints.append(constraint)
 
+    def f_diff_drive(
+        self, x: npt.NDArray[np.float64], u: npt.NDArray[np.float64]
+    ) -> npt.NDArray[np.float64]:
+        """The dynamical model for a differential drive.
+
+        Keyword arguments:
+        x -- state column vector: [x, y, heading, left velocity, right velocity]
+        u -- input column vector: [left voltage, right voltage]
+
+        Returns:
+        dx/dt
+        """
+        v = (x[3, 0] + x[4, 0]) / 2
+
+        dxdt = np.empty((5, 1))
+        dxdt[0, 0] = v * math.cos(x[2, 0])
+        dxdt[1, 0] = v * math.sin(x[2, 0])
+        dxdt[2, 0] = (x[4, 0] - x[3, 0]) / self.trackwidth
+        dxdt[3:5, :] = self.A @ x[3:5, :] + self.B @ u
+        return dxdt
+
+    def generate_initial_path(self, X, U):
+        """Use differential drive LQR to generate the initial path for
+        trajectory optimization.
+        """
+        import control as ct
+        from frccontrol import lqr
+
+        A = np.array(
+            [
+                [0, 0, 0, 0.5, 0.5],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, -1 / self.trackwidth, 1 / self.trackwidth],
+                [0, 0, 0, self.A[0, 0], self.A[0, 1]],
+                [0, 0, 0, self.A[1, 0], self.A[1, 1]],
+            ]
+        )
+        B = np.array(
+            [
+                [0, 0],
+                [0, 0],
+                [0, 0],
+                [self.B[0, 0], self.B[0, 1]],
+                [self.B[1, 0], self.B[1, 1]],
+            ]
+        )
+        Q = np.diag(1.0 / np.square([0.0625, 0.125, 2.5, 0.95, 0.95]))
+        R = np.diag(1.0 / np.square([12, 12]))
+
+        i_waypoint = 0
+        waypoint = self.waypoints[i_waypoint]
+        x = np.array([[waypoint.x], [waypoint.y], [0], [0], [0]])
+        if waypoint.heading is not None:
+            x[2, 0] = waypoint.heading
+
+        i_waypoint += 1
+        waypoint = self.waypoints[i_waypoint]
+        r = np.array([[waypoint.x], [waypoint.y], [0], [0], [0]])
+        if waypoint.heading is not None:
+            r[2, 0] = waypoint.heading
+        u = np.array([[0], [0]])
+        for k in range(X.shape[1] - 1):
+            self.problem.set_initial(X[:, k], x)
+
+            v = (x[3, 0] + x[4, 0]) / 2
+            if abs(v) < 1e-8:
+                v = 1e-8
+            A[1, 2] = v
+            sys = ct.StateSpace(A, B, np.eye(5, 5), np.zeros((5, 2))).sample(self.dt)
+            K = lqr(sys, Q, R)
+            rot = np.array(
+                [
+                    [math.cos(x[2]), math.sin(x[2]), 0, 0, 0],
+                    [-math.sin(x[2]), math.cos(x[2]), 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 1],
+                ]
+            )
+            u = K @ rot @ (r - x)
+            u = np.clip(u, np.array([[-12], [-12]]), np.array([[12], [12]]))
+            self.problem.set_initial(U[:, k], u)
+            x = runge_kutta(self.f_diff_drive, x, u, self.dt)
+
+            if np.allclose(r, x):
+                i_waypoint += 1
+                waypoint = self.waypoints[i_waypoint]
+                r = np.array([[waypoint.x], [waypoint.y], [0], [0], [0]])
+                if waypoint.heading is not None:
+                    r[2, 0] = waypoint.heading
+
     def optimize(self, q: float, r: List[float]):
         """Generate the optimal trajectory.
 
@@ -116,8 +226,7 @@ class DifferentialDriveTrajectoryOptimizer:
         inputs -- matrix of inputs (inputs x times)
         """
         num_segments = len(self.waypoints) - 1
-        vars_per_segment = 100
-        num_vars = vars_per_segment * num_segments
+        num_vars = self.vars_per_segment * num_segments
 
         # States: [x, y, heading, left velocity, right velocity]
         X = self.problem.variable(5, num_vars + 1)
@@ -132,7 +241,7 @@ class DifferentialDriveTrajectoryOptimizer:
             if i == 0:
                 segment_end = 0
             else:
-                segment_end = i * vars_per_segment - 1
+                segment_end = i * self.vars_per_segment - 1
 
             self.problem.subject_to(X[0, segment_end] == waypoint.x)
             self.problem.subject_to(X[1, segment_end] == waypoint.y)
@@ -145,37 +254,14 @@ class DifferentialDriveTrajectoryOptimizer:
                 )
             if i > 0:
                 for constraint in waypoint.constraints:
-                    segment_start = (i - 1) * vars_per_segment
+                    segment_start = (i - 1) * self.vars_per_segment
                     constraint.apply(
                         self.problem,
                         X[:, segment_start:segment_end],
                         U[:, segment_start:segment_end],
                     )
 
-        for segment in range(num_segments):
-            waypoint = self.waypoints[segment]
-            next_waypoint = self.waypoints[segment + 1]
-
-            segment_start = segment * vars_per_segment
-
-            # Set initial guess for poses as linear interpolation between
-            # waypoints
-            for i in range(vars_per_segment):
-                self.problem.set_initial(
-                    X[0, segment_start + i],
-                    lerp(waypoint.x, next_waypoint.x, i / vars_per_segment),
-                )
-                self.problem.set_initial(
-                    X[1, segment_start + i],
-                    lerp(waypoint.y, next_waypoint.y, i / vars_per_segment),
-                )
-                self.problem.set_initial(
-                    X[2, segment_start + i],
-                    math.atan2(
-                        next_waypoint.y - waypoint.y,
-                        next_waypoint.x - waypoint.x,
-                    ),
-                )
+        self.generate_initial_path(X, U)
 
         # Set up duration decision variables
         Ts = []
@@ -186,7 +272,7 @@ class DifferentialDriveTrajectoryOptimizer:
             self.problem.set_initial(T, 1)
             Ts.append(T)
 
-            dt = T / vars_per_segment
+            dt = T / self.vars_per_segment
             dts.append(dt)
 
         # Linear cost on time
@@ -195,14 +281,14 @@ class DifferentialDriveTrajectoryOptimizer:
         # Quadratic cost on control input
         for k in range(num_vars):
             R = np.diag(1 / np.square(r))
-            J += U[:, k].T @ R @ U[:, k] * dts[int(k / vars_per_segment)]
+            J += U[:, k].T @ R @ U[:, k] * dts[int(k / self.vars_per_segment)]
 
         self.problem.minimize(J)
 
         # Dynamics constraint
         for k in range(num_vars):
             # RK4 integration
-            h = dts[int(k / vars_per_segment)]
+            h = dts[int(k / self.vars_per_segment)]
             x_k = X[:, k]
             u_k = U[:, k]
             x_k1 = X[:, k + 1]
@@ -231,7 +317,7 @@ class DifferentialDriveTrajectoryOptimizer:
         # Generate times for time domain plots
         times = [0]
         for k in range(num_vars):
-            times.append(times[-1] + sol.value(dts[int(k / vars_per_segment)]))
+            times.append(times[-1] + sol.value(dts[int(k / self.vars_per_segment)]))
 
         # Resample trajectory at 5 ms period
         return DifferentialDriveTrajectoryOptimizer.resample(
