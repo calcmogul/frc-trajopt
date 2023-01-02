b144a24326e1b1d3226629e20bde60f9eaf3619d
Tyler Veness <calcmogul@gmail.com>
2023-01-02 10:06:32 -0800

    Allow heading wraparound in dynamics constraints

--- wpimath/trajectory/__init__.py
+++ wpimath/trajectory/__init__.py
@@ -137,7 +137,12 @@ class DifferentialDriveTrajectoryOptimizer:
             self.opti.subject_to(X[0, segment_end] == waypoint.x)
             self.opti.subject_to(X[1, segment_end] == waypoint.y)
             if waypoint.heading is not None:
-                self.opti.subject_to(X[2, segment_end] == waypoint.heading)
+                self.opti.subject_to(
+                    ca.cos(X[2, segment_end]) == math.cos(waypoint.heading)
+                )
+                self.opti.subject_to(
+                    ca.sin(X[2, segment_end]) == math.sin(waypoint.heading)
+                )
             if i > 0:
                 for constraint in waypoint.constraints:
                     segment_start = (i - 1) * vars_per_segment
